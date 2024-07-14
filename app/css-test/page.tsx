f0f88d5a54e1db2078d5b6bed52bb9193ea07608
Yuichiro Kei <yuichirokei@gmail.com>
2024-07-14 16:44:44 +0900

    Add style 3

--- app/css-test/page.tsx
+++ app/css-test/page.tsx
@@ -1,5 +1,6 @@
 import style1 from './test1.module.css'
 import style2 from './test2.module.css'
+import style3 from './test3.module.css'
 
 export default function Page() {
   return (
@@ -7,6 +8,7 @@ export default function Page() {
       <h1>CSS Test</h1>
       <div className={style1.test1}>Test1 message.</div>
       <div className={style2.test2}>Test2 message.</div>
+      <div className={style3.test3}>Test3 message.</div>
     </>
   )
 }
