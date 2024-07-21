3a9c43c8f6547de0d4e40bd3dab50bb4b5f422fc
Yuichiro Kei <yuichirokei@gmail.com>
2024-07-21 13:35:44 +0900

    add css

--- app/css-test/page.tsx
+++ app/css-test/page.tsx
@@ -9,6 +9,7 @@ export default function Page() {
       <div className={style1.test1}>Test1 message.</div>
       <div className={style2.test2}>Test2 message.</div>
       <div className={style3.test3}>Test3 message.</div>
+      <div className={style1.test2}>Test4 message.</div>
     </>
   )
 }
